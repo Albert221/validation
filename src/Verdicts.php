4324fc0708f04b3212e9d5cc669625e51f126b57
Albert221 <w.albert221@gmail.com>
2017-12-10 21:25:07 +0100

    Add missing phpDoc

--- src/Verdicts.php
+++ src/Verdicts.php
@@ -4,18 +4,31 @@ declare(strict_types=1);
 
 namespace Albert221\Validation;
 
+use ArrayIterator;
 use Countable;
 use IteratorAggregate;
+use Traversable;
 
 class Verdicts implements Countable, IteratorAggregate
 {
+    /**
+     * @var Verdict[]
+     */
     private $verdicts;
 
+    /**
+     * Verdicts constructor.
+     *
+     * @param Verdict[] $verdicts
+     */
     public function __construct(array $verdicts)
     {
         $this->verdicts = $verdicts;
     }
 
+    /**
+     * @return Verdicts
+     */
     public function passing(): Verdicts
     {
         return $this->filter(function (Verdict $verdict) {
@@ -23,6 +36,9 @@ class Verdicts implements Countable, IteratorAggregate
         });
     }
 
+    /**
+     * @return Verdicts
+     */
     public function failing(): Verdicts
     {
         return $this->filter(function (Verdict $verdict) {
@@ -30,6 +46,11 @@ class Verdicts implements Countable, IteratorAggregate
         });
     }
 
+    /**
+     * @param string $fieldName
+     *
+     * @return Verdicts
+     */
     public function forField(string $fieldName): Verdicts
     {
         return $this->filter(function (Verdict $verdict) use ($fieldName) {
@@ -37,33 +58,58 @@ class Verdicts implements Countable, IteratorAggregate
         });
     }
 
+    /**
+     * @param callable $function
+     *
+     * @return Verdicts
+     */
     public function map(callable $function): Verdicts
     {
         return new static(array_map($function, $this->verdicts));
     }
 
+    /**
+     * @param callable $function
+     *
+     * @return Verdicts
+     */
     public function filter(callable $function): Verdicts
     {
         return new static(array_filter($this->verdicts, $function));
     }
 
+    /**
+     * @param callable $function
+     * @param mixed $initial
+     *
+     * @return Verdicts
+     */
     public function reduce(callable $function, $initial = null): Verdicts
     {
         return new static(array_reduce($this->verdicts, $function, $initial));
     }
 
+    /**
+     * @return Verdict[]
+     */
     public function toArray(): array
     {
         return $this->verdicts;
     }
 
+    /**
+     * @return int
+     */
     public function count(): int
     {
         return count($this->verdicts);
     }
 
-    public function getIterator(): \Traversable
+    /**
+     * @return Traversable
+     */
+    public function getIterator(): Traversable
     {
-        return new \ArrayIterator($this->verdicts);
+        return new ArrayIterator($this->verdicts);
     }
 }
