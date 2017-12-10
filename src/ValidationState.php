67357eb395edf98e57801c3729f5b474f63fdb97
Albert221 <w.albert221@gmail.com>
2017-12-10 21:36:53 +0100

    Cleanup, remove ArrayAccess, slight change to API
    Remove ArrayAccess, because this inconsistent way
    of accessing the verdicts could be misleading and
    cause problems when retriving them.
    
    Also, return Vercicts objects not arrays on the
    ValidationState abstraction layer.

--- src/ValidationState.php
+++ src/ValidationState.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 namespace Albert221\Validation;
 
-use ArrayAccess;
-use OutOfBoundsException;
-use OutOfRangeException;
-
-class ValidationState implements ArrayAccess
+class ValidationState
 {
-    const FLAT = 0;
-    const GROUPED = 1;
-
     /**
      * @var Verdicts
      */
@@ -33,17 +26,7 @@ class ValidationState implements ArrayAccess
      */
     public function isValid(): bool
     {
-        return count($this->verdicts->failing()) === 0;
-    }
-
-    /**
-     * @param int $strategy
-     *
-     * @return Verdict[]
-     */
-    public function all(): array
-    {
-        return $this->verdicts->toArray();
+        return 0 === count($this->verdicts->failing());
     }
 
     /**
@@ -54,75 +37,31 @@ class ValidationState implements ArrayAccess
     public function isFieldValid(string $name): bool
     {
         $verdictsForField = $this->verdicts->forField($name);
-        if ($verdictsForField->count() === 0) {
+        if (0 === $verdictsForField->count()) {
             throw new \InvalidArgumentException(sprintf(
                 'Field "%s" does not exist in the validation state.',
                 $name
             ));
         }
 
-        return $verdictsForField->failing()->count === 0;
+        return 0 === $verdictsForField->failing()->count();
+    }
+
+    /**
+     * @return Verdicts
+     */
+    public function all(): Verdicts
+    {
+        return $this->verdicts;
     }
 
     /**
      * @param string $name
      *
-     * @return Verdict[]
+     * @return Verdicts
      */
-    public function field(string $name): array
+    public function field(string $name): Verdicts
     {
-        return $this->verdicts->forField($name)->toArray();
-    }
-
-    /**
-     * @param int|string $offset
-     *
-     * @return bool
-     */
-    public function offsetExists($offset): bool
-    {
-        if (is_int($offset)) {
-            return isset($this->verdicts->toArray()[$offset]);
-        }
-
-        if (!is_string($offset)) {
-            throw new OutOfRangeException(sprintf(
-                'Offset should be an integer index for FLAT strategy or string for GROUPED, %s given.',
-                is_scalar($offset) ? gettype($offset): get_class($offset)
-            ));
-        }
-
-        return $this->verdicts->forField($offset)->count() > 0;
-    }
-
-    /**
-     * @param int|string $offset
-     *
-     * @return array
-     */
-    public function offsetGet($offset): array
-    {
-        if (!$this->offsetExists($offset)) {
-            throw new OutOfBoundsException(sprintf(
-                'Item with offset "%s" does not exist.',
-                $offset
-            ));
-        }
-
-        if (is_int($offset)) {
-            return $this->all()[$offset];
-        }
-
-        return $this->field($offset);
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        // You cannot set any value.
-    }
-
-    public function offsetUnset($offset)
-    {
-        // You cannot unset any value.
+        return $this->verdicts->forField($name);
     }
 }
