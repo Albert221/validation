d4c26b2263742c9424abafb427d416af7144a50d
Albert221 <w.albert221@gmail.com>
2018-01-02 23:39:13 +0100

    Add Length class doc block

--- src/Rule/Length.php
+++ src/Rule/Length.php
@@ -6,6 +6,9 @@ namespace Albert221\Validation\Rule;
 
 use Albert221\Validation\Rule;
 
+/**
+ * Checks whether given value is of proper length.
+ */
 class Length extends Rule
 {
     protected $message = 'This field must be the proper length.';
